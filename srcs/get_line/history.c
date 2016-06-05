c73f850bf01f63b3673ad5a539a6585915b5017b
gerard_v <valentin.gerard@epitech.eu>
2016-06-05 03:45:17 +0200

    add history norme

--- srcs/get_line/history.c
+++ srcs/get_line/history.c
@@ -13,43 +13,55 @@
 #include "history.h"
 #include "get_line.h"
 
-int		history_func(t_edit *line, t_history *history, char *buff)
+void	key_up_func(t_edit *line, t_history *history, int fd)
 {
   char	*el;
   char	*move_left;
+
+  move_left = tigetstr("cub1");
+  el = tigetstr("el");
+  while (line->len != 0)
+    {
+      write(fd, move_left, my_strlen(move_left));
+      line->len -= 1;
+    }
+  write(fd, el, my_strlen(el));
+  history->current_cmd = history->current_cmd->prev;
+  line->cmd = strdup(history->current_cmd->command);
+  line->len = history->current_cmd->length;
+  my_putstr(line->cmd);
+}
+
+void	key_down_func(t_edit *line, t_history *history, int fd)
+{
+  char	*el;
+  char	*move_left;
+
+  move_left = tigetstr("cub1");
+  el = tigetstr("el");
+  while (line->len != 0)
+    {
+      write(fd, move_left, my_strlen(move_left));
+      line->len -= 1;
+    }
+  write(fd, el, my_strlen(el));
+  history->current_cmd = history->current_cmd->next;
+  line->cmd = strdup(history->current_cmd->command);
+  line->len = history->current_cmd->length;
+  my_putstr(line->cmd);
+}
+
+int		history_func(t_edit *line, t_history *history, char *buff)
+{
   int	fd;
 
   if (line->len != 0 && line->pos != 0)
       return (EXIT_FAILURE);
   fd = open("/dev/tty", O_RDWR);
-  move_left = tigetstr("cub1");
-  el = tigetstr("el");
   if  (buff[2] == 'A' && history->current_cmd != NULL &&
        history->current_cmd->prev != NULL)
-    {
-      while (line->len != 0)
-	{
-	  write(fd, move_left, my_strlen(move_left));
-	  line->len -= 1;
-	}
-      write(fd, el, my_strlen(el));
-      history->current_cmd = history->current_cmd->prev;
-      line->cmd = strdup(history->current_cmd->command);
-      line->len = history->current_cmd->length;
-      my_putstr(line->cmd);
-    }
+    key_up_func(line, history, fd);
   else if (buff[2] == 'B' && history->current_cmd != NULL &&
        history->current_cmd->next != NULL)
-    {
-      while (line->len != 0)
-	{
-	  write(fd, move_left, my_strlen(move_left));
-	  line->len -= 1;
-	}
-      write(fd, el, my_strlen(el));
-      history->current_cmd = history->current_cmd->next;
-      line->cmd = strdup(history->current_cmd->command);
-      line->len = history->current_cmd->length;
-      my_putstr(line->cmd);
-    }
+    key_down_func(line, history, fd);
 }
